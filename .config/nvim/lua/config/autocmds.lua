f03e052b6b668ebbbfc3659dd03c9b7d041ced15
Manfred Egger <em@bluechip.at>
2023-10-04 13:44:13 +0200

    Shiftwidth auf 4 gesetzt, nur für Yaml auf 2

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("indent_2", { clear = true }),
+	pattern = { "yml", "yaml" },
+	callback = function()
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.tabstop = 2
+	end,
+})
