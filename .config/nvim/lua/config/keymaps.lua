9e1f2d979d7594fae5ae601ff981e328c5c775fb
Manfred Egger <em@bluechip.at>
2023-10-05 14:28:11 +0200

    Mehr Anpassungen

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<A-Up>", "<C-A>", { desc = "Increase number", remap = true })
+vim.keymap.set("n", "<A-Down>", "<C-X>", { desc = "Decrease number", remap = true })
