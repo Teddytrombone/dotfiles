76c68a4fec52728344725633f2e91e3ad2a34db6
Manfred Egger <em@bluechip.at>
2024-03-14 14:26:41 +0100

    Sicherung

--- .config/nvim-neo/lua/teddytrombone/plugins/mini.lua
+++ .config/nvim-neo/lua/teddytrombone/plugins/mini.lua
@@ -1,75 +1,87 @@
 return {
-	"echasnovski/mini.nvim",
-
-	keys = {
-		{
-			"<leader>bd",
-			function()
-				local bd = require("mini.bufremove").delete
-				if vim.bo.modified then
-					local choice =
-						vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
-					if choice == 1 then -- Yes
-						vim.cmd.write()
-						bd(0)
-					elseif choice == 2 then -- No
-						bd(0, true)
-					end
-				else
-					bd(0)
-				end
-			end,
-			desc = "Delete Buffer",
-		},
+    "echasnovski/mini.nvim",
+    event = "VeryLazy",
+    keys = {
+        {
+            "<leader>bd",
+            function()
+                local bd = require("mini.bufremove").delete
+                if vim.bo.modified then
+                    local choice =
+                        vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
+                    if choice == 1 then -- Yes
+                        vim.cmd.write()
+                        bd(0)
+                    elseif choice == 2 then -- No
+                        bd(0, true)
+                    end
+                else
+                    bd(0)
+                end
+            end,
+            desc = "Delete Buffer",
+        },
         -- stylua: ignore
         { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
-		{
-			"<leader>up",
-			function()
-				local Util = require("lazy.core.util")
-				vim.g.minipairs_disable = not vim.g.minipairs_disable
-				if vim.g.minipairs_disable then
-					Util.warn("Disabled auto pairs", { title = "Option" })
-				else
-					Util.info("Enabled auto pairs", { title = "Option" })
-				end
-			end,
-			desc = "Toggle auto pairs",
-		},
-	},
-	config = function()
-		require("mini.pairs").setup()
-		-- Better Around/Inside textobjects
-		--
-		-- Examples:
-		--  - va)  - [V]isually select [A]round [)]paren
-		--  - yinq - [Y]ank [I]nside [N]ext [']quote
-		--  - ci'  - [C]hange [I]nside [']quote
-		require("mini.ai").setup({ n_lines = 500 })
+        {
+            "<leader>up",
+            function()
+                local Util = require("lazy.core.util")
+                vim.g.minipairs_disable = not vim.g.minipairs_disable
+                if vim.g.minipairs_disable then
+                    Util.warn("Disabled auto pairs", { title = "Option" })
+                else
+                    Util.info("Enabled auto pairs", { title = "Option" })
+                end
+            end,
+            desc = "Toggle auto pairs",
+        },
+    },
+    config = function()
+        require("mini.pairs").setup()
+        -- Better Around/Inside textobjects
+        --
+        -- Examples:
+        --  - va)  - [V]isually select [A]round [)]paren
+        --  - yinq - [Y]ank [I]nside [N]ext [']quote
+        --  - ci'  - [C]hange [I]nside [']quote
+        local ai = require("mini.ai")
+        ai.setup({
+            n_lines = 500,
+            custom_textobjects = {
+                o = ai.gen_spec.treesitter({
+                    a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+                    i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+                }, {}),
+                f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+                c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+                t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },
+            },
+        })
 
-		-- Add/delete/replace surroundings (brackets, quotes, etc.)
-		--
-		-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-		-- - sd'   - [S]urround [D]elete [']quotes
-		-- - sr)'  - [S]urround [R]eplace [)] [']
-		require("mini.surround").setup()
+        -- Add/delete/replace surroundings (brackets, quotes, etc.)
+        --
+        -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+        -- - sd'   - [S]urround [D]elete [']quotes
+        -- - sr)'  - [S]urround [R]eplace [)] [']
+        require("mini.surround").setup()
 
-		-- Simple and easy statusline.
-		--  You could remove this setup call if you don't like it,
-		--  and try some other statusline plugin
-		-- local statusline = require 'mini.statusline'
-		-- set use_icons to true if you have a Nerd Font
-		-- statusline.setup { use_icons = vim.g.have_nerd_font }
+        -- Simple and easy statusline.
+        --  You could remove this setup call if you don't like it,
+        --  and try some other statusline plugin
+        -- local statusline = require 'mini.statusline'
+        -- set use_icons to true if you have a Nerd Font
+        -- statusline.setup { use_icons = vim.g.have_nerd_font }
 
-		-- You can configure sections in the statusline by overriding their
-		-- default behavior. For example, here we set the section for
-		-- cursor location to LINE:COLUMN
-		---@diagnostic disable-next-line: duplicate-set-field
-		-- statusline.section_location = function()
-		-- return '%2l:%-2v'
-		-- end
+        -- You can configure sections in the statusline by overriding their
+        -- default behavior. For example, here we set the section for
+        -- cursor location to LINE:COLUMN
+        ---@diagnostic disable-next-line: duplicate-set-field
+        -- statusline.section_location = function()
+        -- return '%2l:%-2v'
+        -- end
 
-		-- ... and there is more!
-		--  Check out: https://github.com/echasnovski/mini.nvim
-	end,
+        -- ... and there is more!
+        --  Check out: https://github.com/echasnovski/mini.nvim
+    end,
 }
--- .config/nvim-neo/lua/teddytrombone/utils/lsp.lua
+++ .config/nvim-neo/lua/teddytrombone/utils/lsp.lua
@@ -1,143 +1,149 @@
 local M = {}
 
 M.lsp_kinds = {
-	Array = " ",
-	Boolean = "󰨙 ",
-	Class = " ",
-	Codeium = "󰘦 ",
-	Color = " ",
-	Control = " ",
-	Collapsed = " ",
-	Constant = "󰏿 ",
-	Constructor = " ",
-	Copilot = " ",
-	Enum = " ",
-	EnumMember = " ",
-	Event = " ",
-	Field = " ",
-	File = " ",
-	Folder = " ",
-	Function = "󰊕 ",
-	Interface = " ",
-	Key = " ",
-	Keyword = " ",
-	Method = "󰊕 ",
-	Module = " ",
-	Namespace = "󰦮 ",
-	Null = " ",
-	Number = "󰎠 ",
-	Object = " ",
-	Operator = " ",
-	Package = " ",
-	Property = " ",
-	Reference = " ",
-	Snippet = " ",
-	String = " ",
-	Struct = "󰆼 ",
-	TabNine = "󰏚 ",
-	Text = " ",
-	TypeParameter = " ",
-	Unit = " ",
-	Value = " ",
-	Variable = "󰀫 ",
+    Array = " ",
+    Boolean = "󰨙 ",
+    Class = " ",
+    Codeium = "󰘦 ",
+    Color = " ",
+    Control = " ",
+    Collapsed = " ",
+    Constant = "󰏿 ",
+    Constructor = " ",
+    Copilot = " ",
+    Enum = " ",
+    EnumMember = " ",
+    Event = " ",
+    Field = " ",
+    File = " ",
+    Folder = " ",
+    Function = "󰊕 ",
+    Interface = " ",
+    Key = " ",
+    Keyword = " ",
+    Method = "󰊕 ",
+    Module = " ",
+    Namespace = "󰦮 ",
+    Null = " ",
+    Number = "󰎠 ",
+    Object = " ",
+    Operator = " ",
+    Package = " ",
+    Property = " ",
+    Reference = " ",
+    Snippet = " ",
+    String = " ",
+    Struct = "󰆼 ",
+    TabNine = "󰏚 ",
+    Text = " ",
+    TypeParameter = " ",
+    Unit = " ",
+    Value = " ",
+    Variable = "󰀫 ",
 }
 
 M.mason_packages = {
-	"bash-language-server",
-	"css-lsp",
-	"docker-compose-language-service",
-	"dockerfile-language-server",
-	"eslint-lsp",
-	"hadolint",
-	"html-lsp",
-	"intelephense",
-	"json-lsp",
-	"lua-language-server",
-	"markdownlint",
-	"nginx-language-server",
-	"php-debug-adapter",
-	"phpactor",
-	"phpstan",
-	"prettierd",
-	"shellcheck",
-	"shfmt",
-	"stylua",
-	"yamlfix",
+    "bash-language-server",
+    "css-lsp",
+    "docker-compose-language-service",
+    "dockerfile-language-server",
+    "eslint-lsp",
+    "hadolint",
+    "html-lsp",
+    "intelephense",
+    "json-lsp",
+    "lua-language-server",
+    "markdownlint",
+    "nginx-language-server",
+    "php-debug-adapter",
+    "phpactor",
+    "phpstan",
+    "prettierd",
+    "shellcheck",
+    "shfmt",
+    "stylua",
+    "yamlfix",
 }
 
 M.lsp_servers = {
-	"tsserver",
-	"lua_ls",
-	"eslint",
-	"bashls",
-	"yamlls",
-	"jsonls",
-	"cssls",
-	"html",
-	"intelephense",
-	"phpactor",
+    "tsserver",
+    "lua_ls",
+    "eslint",
+    "bashls",
+    "yamlls",
+    "jsonls",
+    "cssls",
+    "html",
+    "intelephense",
+    "phpactor",
 }
 
 M.php_root_dir = function(pattern)
-	local util = require("lspconfig.util")
-	local cwd = vim.loop.cwd()
-	local root = util.root_pattern("composer.json", ".git")(pattern)
+    local util = require("lspconfig.util")
+    local cwd = vim.loop.cwd()
+    local root = util.root_pattern("composer.json", ".git")(pattern)
 
-	if root == nil then
-		return cwd
-	end
+    if root == nil then
+        return cwd
+    end
 
-	-- prefer cwd if root is a descendant
-	return util.path.is_descendant(cwd, root) and cwd or root
+    -- prefer cwd if root is a descendant
+    return util.path.is_descendant(cwd, root) and cwd or root
 end
 
 M.setup = function()
-	vim.api.nvim_create_autocmd("LspAttach", {
-		group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-		callback = function(args)
-			local bufnr = args.buf
+    vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(args)
+            local bufnr = args.buf
 
-			vim.bo[args.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+            vim.bo[args.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = bufnr, desc = "Goto declaration" })
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, desc = "Goto definition" })
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "Hover Documentation" })
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = bufnr, desc = "Goto implementation" })
-			vim.keymap.set(
-				"n",
-				"<C-k>",
-				vim.lsp.buf.signature_help,
-				{ buffer = bufnr, desc = "Signature documentation" }
-			)
-			vim.keymap.set(
-				"n",
-				"<space>wa",
-				vim.lsp.buf.add_workspace_folder,
-				{ buffer = bufnr, desc = "Add workspace folder" }
-			)
-			vim.keymap.set(
-				"n",
-				"<space>wr",
-				vim.lsp.buf.remove_workspace_folder,
-				{ buffer = bufnr, desc = "Remove workspace folder" }
-			)
-			vim.keymap.set("n", "<space>wl", function()
-				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-			end, { buffer = bufnr, desc = "List workspace folders" })
-			vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, { buffer = bufnr, desc = "Type definition" })
-			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, { buffer = bufnr, desc = "Rename" })
-			vim.keymap.set(
-				{ "n", "v" },
-				"<space>ca",
-				vim.lsp.buf.code_action,
-				{ buffer = bufnr, desc = "Code actions" }
-			)
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = bufnr, desc = "Find references" })
-			vim.keymap.set("n", "<space>f", function()
-				vim.lsp.buf.format({ async = true })
-			end, { buffer = bufnr, desc = "Format" })
-		end,
-	})
+            local builtin = require("telescope.builtin")
+
+            vim.keymap.set("n", "<leader>cl", "<cmd>LspInfo<cr>", { buffer = bufnr, desc = "Lsp Info" })
+            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = bufnr, desc = "Goto declaration" })
+            vim.keymap.set("n", "gd", function() builtin.lsp_definitions({ reuse_win = true }) end,
+                { buffer = bufnr, desc = "Goto definition" })
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "Hover Documentation" })
+            vim.keymap.set("n", "gi", function() builtin.lsp_implementations({ reuse_win = true }) end,
+                { buffer = bufnr, desc = "Goto implementation" })
+            vim.keymap.set(
+                "n",
+                "<C-k>",
+                vim.lsp.buf.signature_help,
+                { buffer = bufnr, desc = "Signature documentation" }
+            )
+            vim.keymap.set(
+                "n",
+                "<space>wa",
+                vim.lsp.buf.add_workspace_folder,
+                { buffer = bufnr, desc = "Add workspace folder" }
+            )
+            vim.keymap.set(
+                "n",
+                "<space>wr",
+                vim.lsp.buf.remove_workspace_folder,
+                { buffer = bufnr, desc = "Remove workspace folder" }
+            )
+            vim.keymap.set("n", "<space>wl", function()
+                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+            end, { buffer = bufnr, desc = "List workspace folders" })
+            vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, { buffer = bufnr, desc = "Type definition" })
+            vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, { buffer = bufnr, desc = "Rename" })
+            vim.keymap.set(
+                { "n", "v" },
+                "<space>ca",
+                vim.lsp.buf.code_action,
+                { buffer = bufnr, desc = "Code actions" }
+            )
+            vim.keymap.set("n", "gr", function() builtin.lsp_references({ reuse_win = true }) end,
+                { buffer = bufnr, desc = "Find references" })
+            vim.keymap.set("n", "<space>f", function()
+                vim.lsp.buf.format({ async = true })
+            end, { buffer = bufnr, desc = "Format" })
+        end,
+    })
 end
 
 return M
