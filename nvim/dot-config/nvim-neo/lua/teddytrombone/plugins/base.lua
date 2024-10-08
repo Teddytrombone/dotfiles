a8b9af6ccb5c8322a38934d0f230d64ed16dfab8
Teddytrombone <github@tmk-stgeorgen.at>
2024-10-08 12:15:41 +0200

    Optimise

--- nvim/dot-config/nvim-neo/lua/teddytrombone/plugins/base.lua
+++ nvim/dot-config/nvim-neo/lua/teddytrombone/plugins/base.lua
@@ -1,8 +1,9 @@
 return {
-	"tpope/vim-fugitive",
-	"tpope/vim-rhubarb",
+    -- Git wrapper
+    -- "tpope/vim-fugitive",
+    -- "tpope/vim-rhubarb",
 
-	-- Detect tabstop and shiftwidth automatically
-	-- "tpope/vim-sleuth",
-	{ "numToStr/Comment.nvim", opts = {} },
+    -- Detect tabstop and shiftwidth automatically
+    -- "tpope/vim-sleuth",
+    -- { "numToStr/Comment.nvim", opts = {} },
 }
--- nvim/dot-config/nvim-neo/lua/teddytrombone/plugins/lsp/nvim-lspconfig.lua
+++ nvim/dot-config/nvim-neo/lua/teddytrombone/plugins/lsp/nvim-lspconfig.lua
@@ -1,62 +1,61 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-        -- event = "BufReadPre",
-        dependencies = {
-            { "williamboman/mason.nvim", config = true },
-            "williamboman/mason-lspconfig.nvim",
-            {
-                "folke/lazydev.nvim",
-                ft = "lua", -- only load on lua files
-                opts = {
-                    library = {
-                        -- See the configuration section for more details
-                        -- Load luvit types when the `vim.uv` word is found
-                        { path = "luvit-meta/library", words = { "vim%.uv" } },
-                    },
-                },
-            },
-            { "Bilal2453/luvit-meta",    lazy = true },
-            -- Useful status updates for LSP
-            -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-            { "j-hui/fidget.nvim",       opts = {} },
-            'hrsh7th/cmp-nvim-lsp',
-        },
-        config = function(_, _)
-            local utils = require("teddytrombone.utils.lsp")
-            local mason_lspconfig = require("mason-lspconfig")
-            local lspconfig = require("lspconfig")
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-            capabilities = vim.tbl_deep_extend('force', capabilities,
-                require("cmp_nvim_lsp").default_capabilities(capabilities))
-            utils.typo3_lsp(lspconfig)
-            utils.setup()
+	{
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		opts = {
+			library = {
+				-- See the configuration section for more details
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "luvit-meta/library", words = { "vim%.uv" } },
+			},
+		},
+	},
+	{ "Bilal2453/luvit-meta", lazy = true },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{ "williamboman/mason.nvim", config = true },
+			"williamboman/mason-lspconfig.nvim",
+			-- Useful status updates for LSP
+			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+			{ "j-hui/fidget.nvim", opts = {} },
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		config = function(_, _)
+			local utils = require("teddytrombone.utils.lsp")
+			local mason_lspconfig = require("mason-lspconfig")
+			local lspconfig = require("lspconfig")
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities =
+				vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities(capabilities))
+			utils.typo3_lsp(lspconfig)
+			utils.setup()
 
-            mason_lspconfig.setup({
-                ensure_installed = utils.lsp_servers,
-            })
+			mason_lspconfig.setup({
+				ensure_installed = utils.lsp_servers,
+			})
 
-            mason_lspconfig.setup_handlers({
-                function(server_name)
-                    lspconfig[server_name].setup({
-                        capabilities = capabilities,
-                    })
-                end,
-                ["intelephense"] = function()
-                    lspconfig.intelephense.setup({
-                        capabilities = capabilities,
-                        root_dir = utils.php_root_dir,
-                    })
-                end,
-                ["phpactor"] = function()
-                    lspconfig.phpactor.setup({
-                        capabilities = capabilities,
-                        root_dir = utils.php_root_dir,
-                    })
-                end,
-            })
+			mason_lspconfig.setup_handlers({
+				function(server_name)
+					lspconfig[server_name].setup({
+						capabilities = capabilities,
+					})
+				end,
+				["intelephense"] = function()
+					lspconfig.intelephense.setup({
+						capabilities = capabilities,
+						root_dir = utils.php_root_dir,
+					})
+				end,
+				["phpactor"] = function()
+					lspconfig.phpactor.setup({
+						capabilities = capabilities,
+						root_dir = utils.php_root_dir,
+					})
+				end,
+			})
 
-            lspconfig.typo3.setup({})
-        end,
-    },
+			lspconfig.typo3.setup({})
+		end,
+	},
 }
