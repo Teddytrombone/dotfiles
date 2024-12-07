5ca3ca9f3593380ac218400f95a24603ee850eaa
Teddytrombone <github@tmk-stgeorgen.at>
2024-12-07 22:39:17 +0100

    Updated LazyVim

--- nvim/dot-config/nvim/lua/config/lazy.lua
+++ nvim/dot-config/nvim/lua/config/lazy.lua
@@ -9,13 +9,13 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
 	spec = {
 		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
-		{ import = "lazyvim.plugins.extras.lang.typescript" },
-		{ import = "lazyvim.plugins.extras.lang.yaml" },
-		{ import = "lazyvim.plugins.extras.lang.docker" },
-		{ import = "lazyvim.plugins.extras.lang.json" },
-		-- { import = "lazyvim.plugins.extras.coding.yanky" },
-		{ import = "lazyvim.plugins.extras.formatting.prettier" },
-		{ import = "lazyvim.plugins.extras.coding.codeium" },
+		-- { import = "lazyvim.plugins.extras.lang.typescript" },
+		-- { import = "lazyvim.plugins.extras.lang.yaml" },
+		-- { import = "lazyvim.plugins.extras.lang.docker" },
+		-- { import = "lazyvim.plugins.extras.lang.json" },
+		-- -- { import = "lazyvim.plugins.extras.coding.yanky" },
+		-- { import = "lazyvim.plugins.extras.formatting.prettier" },
+		-- { import = "lazyvim.plugins.extras.coding.codeium" },
 		{ import = "plugins" },
 	},
 	defaults = {
