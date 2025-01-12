9be4572c4e11407b3529932458666e342d794421
Teddytrombone <github@tmk-stgeorgen.at>
2025-01-12 14:32:19 +0100

    No animations

--- nvim/dot-config/nvim/lua/config/options.lua
+++ nvim/dot-config/nvim/lua/config/options.lua
@@ -7,3 +7,5 @@ vim.opt.shiftwidth = 4
 
 vim.api.nvim_set_hl(0, "TYPO3", { fg = "#FF8700", bg = nil })
 vim.api.nvim_set_hl(0, "GHOSTTY", { fg = "#3551f3", bg = nil })
+
+vim.g.snacks_animate = false
