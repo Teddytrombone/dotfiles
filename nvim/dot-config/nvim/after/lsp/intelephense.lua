1873d000c929cec5abbadef5b71535ee727affaf
Manfred Egger <github@tmk-stgeorgen.at>
2026-02-06 07:43:19 +0100

    Intelephense: neuer Versuch

--- nvim/dot-config/nvim/after/lsp/intelephense.lua
+++ nvim/dot-config/nvim/after/lsp/intelephense.lua
@@ -2,5 +2,11 @@
 return {
 	cmd = { "intelephense", "--stdio" },
 	filetypes = { "php" },
-	root_markers = { ".devel", "typo3_src", ".git", "composer.json" },
+	root_dir = function(bufnr, on_dir)
+		local root_markers = { ".devel", "typo3_src", ".git", "composer.json" }
+		-- We fallback to the current working directory if no project root is found
+		local project_root = vim.fs.root(bufnr, root_markers) or vim.fn.getcwd()
+
+		on_dir(project_root)
+	end,
 }
