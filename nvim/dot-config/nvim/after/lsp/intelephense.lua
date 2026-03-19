c3f34d231da402c306a0f0c8eb7de82ab0f42d00
Manfred Egger <github@tmk-stgeorgen.at>
2026-03-19 07:46:09 +0100

    Bugfix intelephense; Deps

--- nvim/dot-config/nvim/after/lsp/intelephense.lua
+++ nvim/dot-config/nvim/after/lsp/intelephense.lua
@@ -3,9 +3,13 @@ return {
 	cmd = { "intelephense", "--stdio" },
 	filetypes = { "php" },
 	root_dir = function(bufnr, on_dir)
-		local root_markers = { ".devel", "typo3_src", ".git", "composer.json" }
-		-- We fallback to the current working directory if no project root is found
-		local project_root = vim.fs.root(bufnr, root_markers) or vim.fn.getcwd()
+		local project_root = vim.fs.root(bufnr, { ".devel" })
+
+		local root_markers = { "typo3_src", ".git", "composer.json" }
+		if not project_root or project_root == "/var/www" then
+			-- We fallback to the current working directory if no project root is found
+			project_root = vim.fs.root(bufnr, root_markers) or vim.fn.getcwd()
+		end
 
 		on_dir(project_root)
 	end,
